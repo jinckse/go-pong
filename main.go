9f0b4b1f11ac4ffb40167d30f7f47a57d711d46b
Jarrod Nix <jarrodn@ospreyvideo.com>
2026-09-03 14:12:27 -0500

    Add more dependencies and draw scores.

--- main.go
+++ main.go
@@ -3,6 +3,11 @@ package main
 import (
 	"log"
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/vector"
+	"github.com/hajimehoshi/ebiten/v2/text"
+	"image/color"
+	"golang.org/x/image/font/basicfont"
+	"fmt"
 )
 
 const (
@@ -49,7 +54,27 @@ func (g*Game) Layout(outsideWidth, outsideHeight int) (int, int) {
 	return screenWidth, screenHeight
 }
 
-func (g *Game) Draw(screen *ebiten.Image) {}
+func (g *Game) Draw(screen *ebiten.Image) {
+	// Draw paddle
+	vector.DrawFilledRect(screen,
+	float32(g.paddle.X), float32(g.paddle.Y),
+	float32(g.paddle.W), float32(g.paddle.H),
+	color.White, false)
+
+	// Draw ball
+	vector.DrawFilledRect(screen,
+	float32(g.ball.X), float32(g.ball.Y),
+	float32(g.ball.W), float32(g.ball.H),
+	color.White, false)
+
+	// Draw current score
+	scoreStr := "Score: " + fmt.Sprint(g.score)    //X, Y
+	text.Draw(screen, scoreStr, basicfont.Face7x13, 10, 20, color.White)
+
+	// Draw high score
+	highScoreStr := "High Score: " + fmt.Sprint(g.highScore) //X, Y
+	text.Draw(screen, highScoreStr, basicfont.Face7x13, 10, 40, color.White)
+}
 
 func (g *Game) Update() error {
 	return nil
